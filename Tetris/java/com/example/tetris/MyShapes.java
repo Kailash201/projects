f6481dc39e5933dba52a04a2e6b9b47e99c370a2
Kailash201 <109354852+Kailash201@users.noreply.github.com>
2022-07-26 20:36:03 +0800

    Add files via upload
    Tetris coded on android studio (Java)

---
+++ Tetris/java/com/example/tetris/MyShapes.java
@@ -0,0 +1,55 @@
+package com.example.tetris;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MyShapes extends View {
+
+    static Paint fill = new Paint();
+    static Paint stroke = new Paint();
+    static Rect msquare = new Rect();
+
+    public MyShapes(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public void init() {
+        fill.setColor(Color.CYAN);
+        stroke.setStyle(Paint.Style.STROKE);
+        stroke.setColor(Color.BLACK);
+        stroke.setStrokeWidth(3);
+
+
+
+        msquare.left = 0;
+        msquare.top = 0;
+        msquare.right = msquare.left + 50;
+        msquare.bottom = msquare.top + 50;
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawRect(msquare, fill);
+        canvas.drawRect(msquare, stroke);
+
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasure, int heightMeasure) {
+        int width = msquare.width() ;
+        int height = msquare.height();
+        setMeasuredDimension(width, height);
+    }
+
+
+}
